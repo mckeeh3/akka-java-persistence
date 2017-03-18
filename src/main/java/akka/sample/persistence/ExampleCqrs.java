ea8f185e6cf459ea126878ed14dbec43489f4876
Hugh McKee <mckee.hugh@gmail.com>
2017-03-18 16:43:05 -0400

    add start of CQRS query side

--- src/main/java/akka/sample/persistence/ExampleCqrs.java
+++ src/main/java/akka/sample/persistence/ExampleCqrs.java
@@ -1,10 +1,19 @@
 package akka.sample.persistence;
 
+import akka.actor.ActorSystem;
+import akka.persistence.query.PersistenceQuery;
+import akka.persistence.query.javadsl.ReadJournal;
+
 /**
  * Playing with Akka persistence query.
  */
 public class ExampleCqrs {
     public static void main(String[] arguments) {
+        ActorSystem actorSystem = ActorSystem.create("cqrs");
+        PersistenceQuery.get(actorSystem).getReadJournalFor(AccountReadJournal.class, "");
+    }
+
+    private static class AccountReadJournal implements ReadJournal {
 
     }
 }
