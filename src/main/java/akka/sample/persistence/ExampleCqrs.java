10d55534da1a85af41ae83a45cb00dc86a921ca5
Hugh McKee <mckee.hugh@gmail.com>
2017-03-19 04:12:43 -0400

    add journal query

--- src/main/java/akka/sample/persistence/ExampleCqrs.java
+++ src/main/java/akka/sample/persistence/ExampleCqrs.java
@@ -1,8 +1,14 @@
 package akka.sample.persistence;
 
+import akka.NotUsed;
+import akka.actor.AbstractActor;
+import akka.actor.ActorContext;
 import akka.actor.ActorSystem;
 import akka.persistence.query.PersistenceQuery;
 import akka.persistence.query.javadsl.ReadJournal;
+import akka.persistence.query.journal.leveldb.javadsl.LeveldbReadJournal;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Source;
 
 /**
  * Playing with Akka persistence query.
@@ -10,10 +16,23 @@ import akka.persistence.query.javadsl.ReadJournal;
 public class ExampleCqrs {
     public static void main(String[] arguments) {
         ActorSystem actorSystem = ActorSystem.create("cqrs");
-        PersistenceQuery.get(actorSystem).getReadJournalFor(AccountReadJournal.class, "");
+
+        final ActorMaterializer mat = ActorMaterializer.create(actorSystem);
+
+        LeveldbReadJournal queries = PersistenceQuery.get(actorSystem)
+                .getReadJournalFor(LeveldbReadJournal.class, LeveldbReadJournal.Identifier());
+
+        Source<String, NotUsed> source = queries.eventsallPersistenceIds();
+        source.runForeach(event -> System.out.println(event), mat);
     }
 
     private static class AccountReadJournal implements ReadJournal {
 
+    }
+
+    private static class AnActor extends AbstractActor {
+        {
+            context().system().settings().config().getString("");
+        }
     }
 }
